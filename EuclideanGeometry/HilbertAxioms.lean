3f2d37f7402c73ac86f349f4a0b3ce65717645aa
kuotsanhsu <learningstud@gmail.com>
2025-03-02 23:56:09 +0800

    Independence of I1, I2, I3, I4, P

--- EuclideanGeometry/HilbertAxioms.lean
+++ EuclideanGeometry/HilbertAxioms.lean
@@ -12,81 +12,220 @@
 
 -/
 
-abbrev ExistsUnique {α} (p : α → Prop) := ∃ a : α, p a ∧ ∀ b : α, p b → a = b
+namespace Membership
+variable {Point Line} [self : Membership Point Line]
 
-open Lean TSyntax.Compat in
-macro "∃!" xs:explicitBinders ", " b:term : term => expandExplicitBinders ``ExistsUnique xs b
+def Join (A B : Point) := {l : Line // A ∈ l ∧ B ∈ l}
+def Meet (l m : Line) := {A : Point // A ∈ l ∧ A ∈ m}
 
-class Incidence (Points Lines) extends Membership Points Lines where
-  I1 (A B : Points) : A ≠ B → ∃! l : Lines, A ∈ l ∧ B ∈ l
-  I2 (l : Lines) : ∃ A ∈ l, ∃ B ∈ l, A ≠ B
-  I3 : ∃ A : Points, ∃ l : Lines, A ∉ l
+def I1 : Prop := ∀ A B : Point, A ≠ B → ∃ l : Join A B, ∀ m : self.Join A B, m = l
+def I2 : Prop := ∀ l : Line, ∃ A ∈ l, ∃ B ∈ l, A ≠ B
+def I3 : Prop := ∃ A B C : Point, ¬∃ l : Line, A ∈ l ∧ B ∈ l ∧ C ∈ l
+def Parallel (l m : Line) : Prop := l ≠ m → Meet l m → False
+def P : Prop := ∀ A : Point, ∀ l : Line, Subsingleton {m : Line // A ∈ m ∧ Parallel l m}
+
+end Membership
+
+class Incidence (Point Line) extends Membership Point Line where
+  i1 : toMembership.I1
+  i2 : toMembership.I2
+  i3 : toMembership.I3
 
 namespace Incidence
-variable {Points Lines} [self : Incidence Points Lines] {A B : Points}
-
-def Join (A B : Points) := {l : Lines // A ∈ l ∧ B ∈ l}
-
-example (h : A ≠ B) : ∃ l : Join A B, ∀ m : self.Join A B, l = m :=
-  match I1 A B h with
-  | ⟨l, hl, h⟩ => ⟨⟨l, hl⟩, fun ⟨m, hm⟩ => Subtype.ext (h m hm)⟩
-
-example : (∃ l : Join A B, ∀ m : self.Join A B, l = m) → ∃! l : Lines, A ∈ l ∧ B ∈ l
-  | ⟨⟨l, hl⟩, h⟩ => ⟨l, hl, fun m hm => Subtype.ext_iff.mp (h ⟨m, hm⟩)⟩
-
-def Meet (l m : Lines) := {P : Points // P ∈ l ∧ P ∈ m}
+variable {Point Line} [self : Incidence Point Line]
 
 /-- Two distinct lines can have at most one point in common. -/
-example {l m : Lines} (hlm : l ≠ m) (A B : self.Meet l m) : A = B :=
-  Classical.byContradiction fun hAB : A ≠ B => suffices l = m from hlm this
-    have ⟨n, _, h⟩ := self.I1 A.val B.val (hAB ∘ Subtype.ext)
+example {l m : Line} (hlm : l ≠ m) (A B : self.Meet l m) : A = B :=
+  Classical.byContradiction fun hAB : A ≠ B =>
+    suffices l = m from hlm this
+    have ⟨⟨n, _⟩, h⟩ := self.i1 A.val B.val (hAB ∘ Subtype.ext)
     calc l
-      _ = n := h l ⟨A.property.1, B.property.1⟩ |>.symm
-      _ = m := h m ⟨A.property.2, B.property.2⟩
+      _ = n := Subtype.ext_iff.mp <| h ⟨l, A.property.1, B.property.1⟩
+      _ = m := Subtype.ext_iff.mp <| h ⟨m, A.property.2, B.property.2⟩ |>.symm
 
-inductive Points3 | A | B | C
-inductive Lines3 | AB | BC | CA
-example : Incidence Points3 Lines3 where
-  mem
-    | .AB, .A | .AB, .B | .BC, .B | .BC, .C | .CA, .C | .CA, .A => True
-    | _, _ => False
-  I1
-    | .A, .B, _ | .B, .A, _ => ⟨.AB, ⟨trivial, trivial⟩, fun | .AB, _ => rfl⟩
-    | .B, .C, _ | .C, .B, _ => ⟨.BC, ⟨trivial, trivial⟩, fun | .BC, _ => rfl⟩
-    | .C, .A, _ | .A, .C, _ => ⟨.CA, ⟨trivial, trivial⟩, fun | .CA, _ => rfl⟩
-  I2
-    | .AB => ⟨.A, trivial, .B, trivial, nofun⟩
-    | .BC => ⟨.B, trivial, .C, trivial, nofun⟩
-    | .CA => ⟨.C, trivial, .A, trivial, nofun⟩
-  I3 := ⟨.A, .BC, nofun⟩
-
-def Parallel (l m : Lines) : Prop := l ≠ m → self.Meet l m → False
-
-example (l : Lines) : self.Parallel l l
+example (l : Line) : self.Parallel l l
   | (h : l ≠ l), _ => h rfl
-example {l m : Lines} : l ≠ m → self.Meet l m → ¬self.Parallel l m
+example {l m : Line} : l ≠ m → self.Meet l m → ¬self.Parallel l m
   | hlm, P, h => h hlm P
-
 
 end Incidence
 
-/-!
-class Join (α β) where
-  join : α → α → β
-class Meet (α β) where
-  meet : α → α → β
-/- `Mathlib.Order.Notation`: `Max.max`, `Min.min` -/
-@[inherit_doc] infixl:68 " ⊔ " => Join.join
-@[inherit_doc] infixl:69 " ⊓ " => Meet.meet
+class Playfair (Point Line) extends Incidence Point Line where
+  p : toMembership.P
 
+namespace Membership.Independence
 
-structure Incidence' (Points Lines) extends Membership Points Lines where
-  line (A B : Points) : A ≠ B → Lines
-  I1symm {A B} (h : A ≠ B) : line B A h.symm = line A B h
-  I1 {A B} (h : A ≠ B) : A ∈ line A B h ∧ B ∈ line A B h
-  I2 (l : Lines) : ∃ A ∈ l, ∃ B ∈ l, A ≠ B
-  I3 : ∃ A : Points, ∃ l : Lines, A ∉ l
--/
+/- Example 6.1.2 satsifies all -/
+namespace Three
 
-class Playfair (Points Lines) extends Incidence Points Lines where
-  P (A : Points) (l : Lines) : Subsingleton {m : Lines // A ∈ m ∧ toIncidence.Parallel l m}
+inductive Point | A | B | C
+inductive Line | AB | BC | CA
+local instance self : Membership Point Line where
+  mem | .AB, .A | .AB, .B | .BC, .B | .BC, .C | .CA, .C | .CA, .A => True | _, _ => False
+
+example : self.I1
+  | .A, .B, _ | .B, .A, _ => ⟨⟨.AB, trivial, trivial⟩, fun | ⟨.AB, _⟩ => rfl⟩
+  | .B, .C, _ | .C, .B, _ => ⟨⟨.BC, trivial, trivial⟩, fun | ⟨.BC, _⟩ => rfl⟩
+  | .C, .A, _ | .A, .C, _ => ⟨⟨.CA, trivial, trivial⟩, fun | ⟨.CA, _⟩ => rfl⟩
+example : self.I2
+  | .AB => ⟨.A, trivial, .B, trivial, nofun⟩
+  | .BC => ⟨.B, trivial, .C, trivial, nofun⟩
+  | .CA => ⟨.C, trivial, .A, trivial, nofun⟩
+example : self.I3 := ⟨.A, .B, .C, nofun⟩
+example : self.P
+  | .A, .AB => .intro fun
+    | ⟨.AB, _⟩, ⟨.AB, _⟩ => rfl
+    | ⟨.AB, _⟩, ⟨.CA, _, h⟩ | ⟨.CA, _, h⟩, _ => nomatch h nofun ⟨.A, trivial, trivial⟩
+  | .A, .CA => .intro fun
+    | ⟨.CA, _⟩, ⟨.CA, _⟩ => rfl
+    | ⟨.CA, _⟩, ⟨.AB, _, h⟩ | ⟨.AB, _, h⟩, _ => nomatch h nofun ⟨.A, trivial, trivial⟩
+  | .A, .BC => .intro fun
+    | ⟨.AB, _⟩, ⟨.AB, _⟩ => rfl
+    | ⟨.AB, _⟩, ⟨.CA, _, h⟩ | ⟨.CA, _, h⟩, _ => nomatch h nofun ⟨.C, trivial, trivial⟩
+  | .B, .AB => .intro fun
+    | ⟨.AB, _⟩, ⟨.AB, _⟩ => rfl
+    | ⟨.AB, _⟩, ⟨.BC, _, h⟩ | ⟨.BC, _, h⟩, _ => nomatch h nofun ⟨.B, trivial, trivial⟩
+  | .B, .BC => .intro fun
+    | ⟨.BC, _⟩, ⟨.BC, _⟩ => rfl
+    | ⟨.BC, _⟩, ⟨.AB, _, h⟩ | ⟨.AB, _, h⟩, _ => nomatch h nofun ⟨.B, trivial, trivial⟩
+  | .B, .CA => .intro fun
+    | ⟨.AB, _⟩, ⟨.AB, _⟩ => rfl
+    | ⟨.AB, _⟩, ⟨.BC, _, h⟩ | ⟨.BC, _, h⟩, _ => nomatch h nofun ⟨.C, trivial, trivial⟩
+  | .C, .AB => .intro fun
+    | ⟨.CA, _⟩, ⟨.CA, _⟩ => rfl
+    | ⟨.CA, _⟩, ⟨.BC, _, h⟩ | ⟨.BC, _, h⟩, _ => nomatch h nofun ⟨.B, trivial, trivial⟩
+  | .C, .BC => .intro fun
+    | ⟨.BC, _⟩, ⟨.BC, _⟩ => rfl
+    | ⟨.BC, _⟩, ⟨.CA, _, h⟩ | ⟨.CA, _, h⟩, _ => nomatch h nofun ⟨.C, trivial, trivial⟩
+  | .C, .CA => .intro fun
+    | ⟨.CA, _⟩, ⟨.CA, _⟩ => rfl
+    | ⟨.CA, _⟩, ⟨.BC, _, h⟩ | ⟨.BC, _, h⟩, _ => nomatch h nofun ⟨.C, trivial, trivial⟩
+
+end Three
+
+/- Example 6.1.3 does not satisfy P. -/
+namespace Five
+
+inductive Point | A | B | C | D | E
+inductive Line | AB | BC | CD | DE | EA | AC | CE | EB | BD | DA
+local instance self : Membership Point Line where
+  mem
+    | .AB, .A | .EA, .A | .AC, .A | .DA, .A
+    | .AB, .B | .BC, .B | .EB, .B | .BD, .B
+    | .BC, .C | .CD, .C | .AC, .C | .CE, .C
+    | .CD, .D | .DE, .D | .BD, .D | .DA, .D
+    | .DE, .E | .EA, .E | .CE, .E | .EB, .E => True
+    | _, _ => False
+
+example : self.I1
+  | .A, .B, _ | .B, .A, _ => ⟨⟨.AB, trivial, trivial⟩, fun | ⟨.AB, _⟩ => rfl⟩
+  | .B, .C, _ | .C, .B, _ => ⟨⟨.BC, trivial, trivial⟩, fun | ⟨.BC, _⟩ => rfl⟩
+  | .C, .D, _ | .D, .C, _ => ⟨⟨.CD, trivial, trivial⟩, fun | ⟨.CD, _⟩ => rfl⟩
+  | .D, .E, _ | .E, .D, _ => ⟨⟨.DE, trivial, trivial⟩, fun | ⟨.DE, _⟩ => rfl⟩
+  | .E, .A, _ | .A, .E, _ => ⟨⟨.EA, trivial, trivial⟩, fun | ⟨.EA, _⟩ => rfl⟩
+  | .A, .C, _ | .C, .A, _ => ⟨⟨.AC, trivial, trivial⟩, fun | ⟨.AC, _⟩ => rfl⟩
+  | .C, .E, _ | .E, .C, _ => ⟨⟨.CE, trivial, trivial⟩, fun | ⟨.CE, _⟩ => rfl⟩
+  | .E, .B, _ | .B, .E, _ => ⟨⟨.EB, trivial, trivial⟩, fun | ⟨.EB, _⟩ => rfl⟩
+  | .B, .D, _ | .D, .B, _ => ⟨⟨.BD, trivial, trivial⟩, fun | ⟨.BD, _⟩ => rfl⟩
+  | .D, .A, _ | .A, .D, _ => ⟨⟨.DA, trivial, trivial⟩, fun | ⟨.DA, _⟩ => rfl⟩
+example : self.I2
+  | .AB => ⟨.A, trivial, .B, trivial, nofun⟩
+  | .BC => ⟨.B, trivial, .C, trivial, nofun⟩
+  | .CD => ⟨.C, trivial, .D, trivial, nofun⟩
+  | .DE => ⟨.D, trivial, .E, trivial, nofun⟩
+  | .EA => ⟨.E, trivial, .A, trivial, nofun⟩
+  | .AC => ⟨.A, trivial, .C, trivial, nofun⟩
+  | .CE => ⟨.C, trivial, .E, trivial, nofun⟩
+  | .EB => ⟨.E, trivial, .B, trivial, nofun⟩
+  | .BD => ⟨.B, trivial, .D, trivial, nofun⟩
+  | .DA => ⟨.D, trivial, .A, trivial, nofun⟩
+example : self.I3 := ⟨.A, .B, .C, nofun⟩
+example : ¬self.P | h => have ⟨h⟩ := h .A .DE; nomatch h ⟨.AB, trivial, nofun⟩ ⟨.AC, trivial, nofun⟩
+
+end Five
+
+/- Model does not satisfy I3. -/
+namespace Two
+
+local instance self : Membership Bool Unit where
+  mem _ _ := True
+
+example : self.I1 | _, _, _ => ⟨⟨(), trivial, trivial⟩, fun _ => rfl⟩
+example : self.I2 | _ => ⟨true, trivial, false, trivial, nofun⟩
+example : ¬self.I3 | ⟨_, _, _, h⟩ => h ⟨(), trivial, trivial, trivial⟩
+example : self.P | _, _ => .intro fun ⟨_, _⟩ _ => rfl
+
+end Two
+
+/- Model does not satisfy I2. -/
+namespace Four
+
+inductive Point | A | B | C
+inductive Line | A | AB | BC | CA
+local instance self : Membership Point Line where
+  mem | .A, .A | .AB, .A | .AB, .B | .BC, .B | .BC, .C | .CA, .C | .CA, .A => True | _, _ => False
+
+example : self.I1
+  | .A, .B, _ | .B, .A, _ => ⟨⟨.AB, trivial, trivial⟩, fun | ⟨.AB, _⟩ => rfl⟩
+  | .B, .C, _ | .C, .B, _ => ⟨⟨.BC, trivial, trivial⟩, fun | ⟨.BC, _⟩ => rfl⟩
+  | .C, .A, _ | .A, .C, _ => ⟨⟨.CA, trivial, trivial⟩, fun | ⟨.CA, _⟩ => rfl⟩
+example : ¬self.I2 := (nomatch · .A)
+example : self.I3 := ⟨.A, .B, .C, nofun⟩
+example : self.P
+  | .A, .A => .intro fun
+    | ⟨.A, _⟩, ⟨.A, _⟩ => rfl
+    | ⟨.A, _⟩, ⟨.AB, _, h⟩ | ⟨.AB, _, h⟩, _
+    | ⟨.A, _⟩, ⟨.CA, _, h⟩ | ⟨.CA, _, h⟩, _ => nomatch h nofun ⟨.A, trivial, trivial⟩
+  | .A, .AB => .intro fun
+    | ⟨.AB, _⟩, ⟨.AB, _⟩ => rfl
+    | ⟨.AB, _⟩, ⟨.CA, _, h⟩ | ⟨.CA, _, h⟩, _
+    | ⟨.AB, _⟩, ⟨.A, _, h⟩ | ⟨.A, _, h⟩, _ => nomatch h nofun ⟨.A, trivial, trivial⟩
+  | .A, .CA => .intro fun
+    | ⟨.CA, _⟩, ⟨.CA, _⟩ => rfl
+    | ⟨.CA, _⟩, ⟨.AB, _, h⟩ | ⟨.AB, _, h⟩, _
+    | ⟨.CA, _⟩, ⟨.A, _, h⟩ | ⟨.A, _, h⟩, _ => nomatch h nofun ⟨.A, trivial, trivial⟩
+  | .A, .BC => .intro fun
+    | ⟨.A, _⟩, ⟨.A, _⟩ => rfl
+    | ⟨.A, _⟩, ⟨.AB, _, h⟩ | ⟨.AB, _, h⟩, _ => nomatch h nofun ⟨.B, trivial, trivial⟩
+    | ⟨.A, _⟩, ⟨.CA, _, h⟩ | ⟨.CA, _, h⟩, _ => nomatch h nofun ⟨.C, trivial, trivial⟩
+  | .B, .A => .intro fun
+    | ⟨.BC, _⟩, ⟨.BC, _⟩ => rfl
+    | ⟨.BC, _⟩, ⟨.AB, _, h⟩ | ⟨.AB, _, h⟩, _ => nomatch h nofun ⟨.A, trivial, trivial⟩
+  | .B, .AB => .intro fun
+    | ⟨.AB, _⟩, ⟨.AB, _⟩ => rfl
+    | ⟨.AB, _⟩, ⟨.BC, _, h⟩ | ⟨.BC, _, h⟩, _ => nomatch h nofun ⟨.B, trivial, trivial⟩
+  | .B, .BC => .intro fun
+    | ⟨.BC, _⟩, ⟨.BC, _⟩ => rfl
+    | ⟨.BC, _⟩, ⟨.AB, _, h⟩ | ⟨.AB, _, h⟩, _ => nomatch h nofun ⟨.B, trivial, trivial⟩
+  | .B, .CA => .intro fun
+    | ⟨.AB, _⟩, ⟨.AB, _⟩ => rfl
+    | ⟨.AB, _⟩, ⟨.BC, _, h⟩ | ⟨.BC, _, h⟩, _ => nomatch h nofun ⟨.C, trivial, trivial⟩
+  | .C, .A => .intro fun
+    | ⟨.BC, _⟩, ⟨.BC, _⟩ => rfl
+    | ⟨.BC, _⟩, ⟨.CA, _, h⟩ | ⟨.CA, _, h⟩, _ => nomatch h nofun ⟨.A, trivial, trivial⟩
+  | .C, .AB => .intro fun
+    | ⟨.CA, _⟩, ⟨.CA, _⟩ => rfl
+    | ⟨.CA, _⟩, ⟨.BC, _, h⟩ | ⟨.BC, _, h⟩, _ => nomatch h nofun ⟨.B, trivial, trivial⟩
+  | .C, .BC => .intro fun
+    | ⟨.BC, _⟩, ⟨.BC, _⟩ => rfl
+    | ⟨.BC, _⟩, ⟨.CA, _, h⟩ | ⟨.CA, _, h⟩, _ => nomatch h nofun ⟨.C, trivial, trivial⟩
+  | .C, .CA => .intro fun
+    | ⟨.CA, _⟩, ⟨.CA, _⟩ => rfl
+    | ⟨.CA, _⟩, ⟨.BC, _, h⟩ | ⟨.BC, _, h⟩, _ => nomatch h nofun ⟨.C, trivial, trivial⟩
+
+end Four
+
+/- Model does not satisfy I1. -/
+namespace Discrete
+
+local instance self : Membership (Fin 3) Empty where
+  mem _ _ := False
+
+example : ¬self.I1 := (nomatch · 0 1 nofun)
+example : self.I2 := nofun
+example : self.I3 := ⟨0, 1, 2, nofun⟩
+example : self.P := nofun
+
+end Discrete
+
+end Membership.Independence
