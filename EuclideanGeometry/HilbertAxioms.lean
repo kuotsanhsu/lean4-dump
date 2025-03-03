45b02d133ba51ac1f733be99055229012e1b91a0
kuotsanhsu <learningstud@gmail.com>
2025-03-03 18:03:58 +0800

    Betweenness

--- EuclideanGeometry/HilbertAxioms.lean
+++ EuclideanGeometry/HilbertAxioms.lean
@@ -17,10 +17,12 @@ variable {Point Line} [self : Membership Point Line]
 
 def Join (A B : Point) := {l : Line // A ∈ l ∧ B ∈ l}
 def Meet (l m : Line) := {A : Point // A ∈ l ∧ A ∈ m}
+def Colinear (A B C : Point) := ∃ l : Line, A ∈ l ∧ B ∈ l ∧ C ∈ l
+def Disjoint (A B C : Point) (l : Line) := A ∉ l ∧ B ∉ l ∧ C ∉ l
 
 def I1 : Prop := ∀ A B : Point, A ≠ B → ∃ l : Join A B, ∀ m : self.Join A B, m = l
 def I2 : Prop := ∀ l : Line, ∃ A ∈ l, ∃ B ∈ l, A ≠ B
-def I3 : Prop := ∃ A B C : Point, ¬∃ l : Line, A ∈ l ∧ B ∈ l ∧ C ∈ l
+def I3 : Prop := ∃ A B C : Point, ¬self.Colinear A B C
 def Parallel (l m : Line) : Prop := l ≠ m → Meet l m → False
 def P : Prop := ∀ A : Point, ∀ l : Line, Subsingleton {m : Line // A ∈ m ∧ Parallel l m}
 
@@ -140,7 +142,7 @@ example : self.I2
   | .BD => ⟨.B, trivial, .D, trivial, nofun⟩
   | .DA => ⟨.D, trivial, .A, trivial, nofun⟩
 example : self.I3 := ⟨.A, .B, .C, nofun⟩
-example : ¬self.P | h => have ⟨h⟩ := h .A .DE; nomatch h ⟨.AB, trivial, nofun⟩ ⟨.AC, trivial, nofun⟩
+example : ¬self.P | h => nomatch (h .A .DE).elim ⟨.AB, trivial, nofun⟩ ⟨.AC, trivial, nofun⟩
 
 end Five
 
@@ -229,3 +231,28 @@ example : self.P := nofun
 end Discrete
 
 end Membership.Independence
+
+section
+variable {α} (p : α → α → α → Prop) (a b c : α)
+
+def Distinct := a ≠ b ∧ b ≠ c ∧ c ≠ a
+def AtLeast1 := p a b c ∨ p b c a ∨ p c a b
+def AtMost1 := ¬(p a b c ∧ p b c a) ∧ ¬(p b c a ∧ p c a b) ∧ ¬(p c a b ∧ p a b c)
+def Exactly1 := AtLeast1 p a b c ∧ AtMost1 p a b c
+-- def Exactly1 :=
+--   p a b c ∧ ¬p b c a ∧ ¬p c a b ∨
+--   ¬p a b c ∧ p b c a ∧ ¬p c a b ∨
+--   ¬p a b c ∧ ¬p b c a ∧ p c a b
+
+end
+
+class Betweenness (Point Line) extends Incidence Point Line where
+  /-- `between A B C` denotes that `B` is between `A` and `C`. -/
+  between (A B C : Point) : Prop
+  b1 A B C : between A B C → toIncidence.Colinear A B C ∧ between C B A
+  b2 A B : A ≠ B → ∃ C, between A B C
+  b3 A B C : Distinct A B C → toIncidence.Colinear A B C → Exactly1 between A B C
+  /-- Pasch's axiom. -/
+  b4 A B C l : ¬toIncidence.Colinear A B C → ¬toIncidence.Disjoint A B C l →
+    (∃ D ∈ l, between A D B) →
+    (∃ P ∈ l, between A P C ∨ between B P C) ∧ ∀ P ∈ l, ∀ Q ∈ l, ¬(between A P C ∧ between B Q C)
